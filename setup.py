6b484078794dbb6e78d1d7425b1027ff4dce6cea
AMarsel2551 <m.adbullinn@gmail.com>
2024-12-29 10:45:49 +0300

    add __init__

--- setup.py
+++ setup.py
@@ -3,34 +3,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="Cache",
-    version="0.1.0",  
-    author="Marsel",
-    author_email="m.adbullinn@example.com",
-    description="...",
-    long_description="",
-    long_description_content_type="text/markdown",  
-    url="https://github.com/AMarsel2551/cache.git",
+    version="0.1.0",
     packages=find_packages(where="cache"),
-    package_dir={"": "cache"},
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.7",  
     install_requires=[
         "zstandard==0.23.0",
     ],
-    extras_require={
-        "dev": [
-            "zstandard==0.23.0",
-        ],
-    },
-    entry_points={
-        "console_scripts": [
-            "example-cli=example_library.cli:main",  
-        ],
-    },
-    include_package_data=True,  
-    license="MIT",  
 )
