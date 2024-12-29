15a9ad9fe9b034a0d1afbe8a591d3b3b0e4354be
AMarsel2551 <m.adbullinn@gmail.com>
2024-12-29 10:46:50 +0300

    add __init__

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 setup(
     name="Cache",
     version="0.1.0",
-    packages=find_packages(where="cache"),
+    packages=find_packages(),
     install_requires=[
         "zstandard==0.23.0",
     ],
