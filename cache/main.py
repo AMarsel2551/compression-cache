40fc0f64d8532e7e25f5a08505975e8d158b3015
AMarsel2551 <m.adbullinn@gmail.com>
2024-12-29 14:22:02 +0300

    add update

--- cache/main.py
+++ cache/main.py
@@ -1,8 +1,9 @@
-import time, pickle
+import pickle
+import time
+from inspect import iscoroutinefunction, signature
+from typing import Any, Awaitable, Callable, Dict, List, Optional, Tuple, Union
+
 from zstandard import ZstdCompressor, ZstdDecompressor
-from typing import Optional, Callable, List, Any, Union
-from inspect import signature, iscoroutinefunction
-from typing import Dict, Tuple
 
 
 class CacheTTL:
@@ -14,30 +15,30 @@ class CacheTTL:
 
         """ Внутренние параметры """
         self.data: Any = None
-        self.key: Union[Tuple[Any, ...], None] = None
+        self.key: Union[int, None] = None
         self.compressor: ZstdCompressor = ZstdCompressor(level=compressor_level or 3)
         self.decompressor: ZstdDecompressor = ZstdDecompressor()
-        self.cache: Dict[Tuple[Any, ...], Any] = {}
-        self.timestamps: Dict[Tuple[Any, ...], float] = {}
+        self.cache: Dict[int, Any] = {}
+        self.timestamps: Dict[int, float] = {}
 
-    def __call__(self, func: Callable, *args, **kwargs) -> Callable:
+    def __call__(self, func: Callable[..., Any], *args: Tuple[Any], **kwargs: Dict[str, Any]) -> Union[Callable[..., Any], Awaitable[Any]]:
         self.func = func
 
-        # For synchronous functions
-        if iscoroutinefunction(self.func):
-            async def wrapper(*args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> Any:
+        # Для синхронных и асинхронных функций
+        def wrapper(*args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> Any:  # Добавлен возвращаемый тип Any
+            if iscoroutinefunction(self.func):
                 self.data = self.checking_the_cache(*args, **kwargs)
                 if self.data:
                     return self.data
 
-                self.data = await func(*args, **kwargs)
-                return self.saving_the_cache()
+                # Асинхронная функция, с аннотацией
+                async def async_func(*args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> Any:  # Указан тип возвращаемого значения
+                    self.data = await func(*args, **kwargs)
+                    return self.saving_the_cache()
 
-            return wrapper
+                return async_func(*args, **kwargs)
 
-        # For asynchronous functions
-        else:
-            def wrapper(*args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> Any:
+            else:
                 self.data = self.checking_the_cache(*args, **kwargs)
                 if self.data:
                     return self.data
@@ -45,7 +46,7 @@ class CacheTTL:
                 self.data = func(*args, **kwargs)
                 return self.saving_the_cache()
 
-            return wrapper
+        return wrapper
 
     def checking_the_cache(self, *args: Tuple[Any, ...], **kwargs: Dict[str, Any]) -> Any:
         self.key = self.generate_key(*args, **kwargs)
@@ -59,43 +60,24 @@ class CacheTTL:
     def saving_the_cache(self) -> Any:
         if self.compressor_level:
             self.compress_data()
-        self.cache[self.key] = self.data
-        self.timestamps[self.key] = time.time()
-        self.decompress_data()
+        if self.key:
+            self.cache[self.key] = self.data
+            self.timestamps[self.key] = time.time()
+        if self.compressor_level:
+            self.decompress_data()
         return self.data
 
-    def make_hashable(self, value: Any) -> Any:
-        """ Преобразует значение в хэшируемый формат """
-        if isinstance(value, dict):
-            return tuple(sorted((k, self.make_hashable(v)) for k, v in value.items()))
-        if isinstance(value, list):
-            return tuple(self.make_hashable(v) for v in value)
-        if hasattr(value, "__dict__"):  # Преобразуем объект с атрибутами
-            return tuple(sorted((k, self.make_hashable(v)) for k, v in vars(value).items()))
-        return value
-
-    def generate_key(self, *args, **kwargs) -> Tuple[Any, ...]:
+    def generate_key(self, *args: Tuple[Any], **kwargs: Dict[Any, Any]) -> int:
         """ Генерирует ключ для кеша на основе указанных аргументов """
         sig = signature(self.func)
         bound_args = sig.bind(*args, **kwargs)
-
         bound_args.apply_defaults()
-
-        if self.key_args is None:
-            return tuple(sorted((k, self.make_hashable(value=value)) for k, value in bound_args.arguments.items()))
-
-        key = {
-            arg_name: self.make_hashable(value=bound_args.arguments[arg_name])
-            for arg_name in self.key_args
-            if arg_name in bound_args.arguments
-        }
-        return tuple(sorted(key.items()))
+        return hash(str(bound_args))
 
     def compress_data(self) -> None:
-        """ Сжатие кэшированного обьекта """
+        """ Сжатие кэшированного объекта """
         self.data = self.compressor.compress(pickle.dumps(self.data))
 
     def decompress_data(self) -> None:
-        """ Распаковывания кэшированного обьекта """
+        """ Распаковывание кэшированного объекта """
         self.data = pickle.loads(self.decompressor.decompress(self.data))
-
--- setup.py
+++ setup.py
@@ -1,9 +1,8 @@
-from setuptools import setup, find_packages
-
+from setuptools import find_packages, setup
 
 setup(
     name="Cache",
-    version="0.1.1",
+    version="0.1.2",
     packages=find_packages(),
     install_requires=[
         "zstandard==0.23.0",
