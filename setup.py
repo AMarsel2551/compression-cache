fb9dc93dbdcd9b1cffeb4e860a0f7b226c2add7c
AMarsel2551 <m.adbullinn@gmail.com>
2025-01-09 17:47:07 +0300

    add update

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="Cache",
+    name="ecomcache",
     version="0.1.3",
     packages=find_packages(),
     install_requires=["zstandard==0.23.0"],
