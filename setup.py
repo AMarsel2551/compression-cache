d4266bd9f0baba59ee5e2710e925925bad22d66f
AMarsel2551 <m.adbullinn@gmail.com>
2025-01-09 17:40:31 +0300

    add update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import find_packages, setup
 
 setup(
     name="Cache",
-    version="0.1.2",
+    version="0.1.3",
     packages=find_packages(),
     install_requires=["zstandard==0.23.0"],
 )
