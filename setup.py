45baef29574ce3509ac6bb4309b542bc8975c708
marsel <m.adbullinn@gmail.com>
2025-04-09 11:26:18 +0300

    edit

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import find_packages, setup
 
 setup(
     name="compression-cache",
-    version="0.0.2",
+    version="0.0.3",
     packages=find_packages(),
     description='Python function caching with compression',
     install_requires=["zstandard==0.23.0"],
     author_email="m.adbullinn@gmail.com",
     zip_safe=False,
-    url="https://github.com/AMarsel2551/cache.git"
+    url="https://github.com/AMarsel2551/compression-cache.git"
 )
