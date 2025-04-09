9269d93177983c6fb556d6d8ea7d46cbe435bd89
marsel <m.adbullinn@gmail.com>
2025-04-09 12:13:52 +0300

    edit

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ def read_requirements():
 
 setup(
     name="compression-cache",
-    version="0.0.5",
+    version="0.0.6",
     packages=find_packages(),
     description='Python function caching with compression',
     long_description=read_readme(),
