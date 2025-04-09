38aff39f738458e7d8cde17e799f1b1edde0ec3b
marsel <m.adbullinn@gmail.com>
2025-04-09 12:00:08 +0300

    edit

--- setup.py
+++ setup.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import os
 from setuptools import find_packages, setup
+
+
+def read_readme():
+    with open(os.path.join(os.path.dirname(__file__), 'README.md'), encoding='utf-8') as f:
+        return f.read()
+
 
 setup(
     name="compression-cache",
     version="0.0.4",
     packages=find_packages(),
     description='Python function caching with compression',
+    long_description=read_readme(),
+    long_description_content_type="text/markdown",
     install_requires=["zstandard==0.23.0"],
     author_email="m.adbullinn@gmail.com",
     zip_safe=False,
