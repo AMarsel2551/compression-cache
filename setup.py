4e1840c716f86dd619ba7ebdc4d339a4877585e8
marsel <m.adbullinn@gmail.com>
2025-04-09 10:34:47 +0300

    bug fix

--- setup.py
+++ setup.py
@@ -4,8 +4,11 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="ecomcache",
-    version="0.1.5",
+    name="cache",
+    version="0.0.1",
     packages=find_packages(),
+    description='Cache',
     install_requires=["zstandard==0.23.0"],
+    author_email="m.adbullinn@gmail.com",
+    zip_safe=False,
 )
