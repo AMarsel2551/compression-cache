39783af7ac3f9c8c17619a2abe060213769aa687
marsel <m.adbullinn@gmail.com>
2025-04-09 12:01:14 +0300

    edit

--- setup.py
+++ setup.py
@@ -10,6 +10,11 @@ def read_readme():
         return f.read()
 
 
+def read_requirements():
+    with open(os.path.join(os.path.dirname(__file__), 'requirements.txt'), encoding='utf-8') as f:
+        return f.read().splitlines()
+
+
 setup(
     name="compression-cache",
     version="0.0.4",
@@ -17,7 +22,7 @@ setup(
     description='Python function caching with compression',
     long_description=read_readme(),
     long_description_content_type="text/markdown",
-    install_requires=["zstandard==0.23.0"],
+    install_requires=read_requirements(),
     author_email="m.adbullinn@gmail.com",
     zip_safe=False,
     url="https://github.com/AMarsel2551/compression-cache.git"
