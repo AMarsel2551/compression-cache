05db4f0fccbdfc33d1ecdf400996c8270faf5e51
marsel <m.adbullinn@gmail.com>
2025-04-09 10:41:51 +0300

    bug fix

--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="cache",
+    name="super_cache",
     version="0.0.1",
     packages=find_packages(),
-    description='Cache',
+    description='Cache function',
     install_requires=["zstandard==0.23.0"],
     author_email="m.adbullinn@gmail.com",
     zip_safe=False,
